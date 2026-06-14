97ea299b8d81567a7695efc89c5d160bbec04c4e
Alvaro M. Valdez <124842688+CurienDev@users.noreply.github.com>
2026-06-14 15:19:32 -0300

    actualizaccion swagger, docs+informes

--- routes/v1/informesRoutes.js
+++ routes/v1/informesRoutes.js
@@ -4,6 +4,76 @@ import { generarPDFEstadisticas } from '../../controllers/informesController.js'
 
 const router = Router();
 
+/**
+ * @swagger
+ * /api/v1/informes/estadisticas:
+ *   get:
+ *     tags:
+ *       - Informes
+ *     summary: Generar y descargar un PDF con estadísticas de turnos.
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: PDF generado correctamente.
+ *         content:
+ *           application/pdf:
+ *             schema:
+ *               type: string
+ *               format: binary
+ *       400:
+ *         description: Solicitud inválida.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 estado:
+ *                   type: boolean
+ *                   example: false
+ *                 mensaje:
+ *                   type: string
+ *                   example: 'Solicitud inválida.'
+ *       401:
+ *         description: No autorizado.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 estado:
+ *                   type: boolean
+ *                   example: false
+ *                 mensaje:
+ *                   type: string
+ *                   example: 'No autorizado.'
+ *       403:
+ *         description: Acceso denegado.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 estado:
+ *                   type: boolean
+ *                   example: false
+ *                 mensaje:
+ *                   type: string
+ *                   example: 'Acceso denegado.'
+ *       500:
+ *         description: Error interno.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 estado:
+ *                   type: boolean
+ *                   example: false
+ *                 mensaje:
+ *                   type: string
+ *                   example: 'Error interno.'
+ */
 router.get('/estadisticas',
     autorizarUsuarios([3]),
     generarPDFEstadisticas
